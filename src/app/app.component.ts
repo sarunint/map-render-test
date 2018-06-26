028755577df91e29f2ccb5ccf68fd23af76a91d6
Sarun Intaralawan <sarunint@sarunint.com>
2018-06-27 00:27:23 +0700

    feat: add 2000 markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  points: Observable<{ latitude: number; longitude: number }[]>;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.points = this.http.get<{ latitude: number; longitude: number }[]>(
+      'https://cdn.rawgit.com/sarunint/ffdee3042404e8b0c78a610a540335c6/raw/df43bb835a593d6fcee726109af84f0b553947e6/points.json'
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAunCzIJDn_fcY_--Rfh5GWUVPOt8mM5fU',
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
